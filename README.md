c4043546cb0aed5e86b5782c11e2cd9d327b11fd
Xenorohl <v.bussat@icloud.com>
2026-05-26 21:38:20 +0200

    commit

--- README.md
+++
@@ -1,1 +0,0 @@
-Pour la partie graphes c'est tout dans "statistics". Et pour lancer le script correctement il faut passer par le dossier "installations" et tout faire runner depuis là
--- installations/source.r
+++
@@ -1,9 +0,0 @@
-source("./source/bioassay.r")
-
-source("./source/leaf_damage.r")
-
-source("./source/phytohormones.r")
-
-source("./source/gcms.r")
-
-source("./source/statistics.r")
